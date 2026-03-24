dd6e9634b1146a36ee35ead8506c35a4e24a2615
Jordanj9 <jordan_j9@hotmail.com>
2026-03-24 16:44:25 -0500

    Add E2E MongoDB configuration and integration tests for books API

--- api/e2e/books.e2e.js
+++ api/e2e/books.e2e.js
@@ -1,35 +1,52 @@
-const mockGetAll = jest.fn();
 const request = require('supertest');
+const { MongoClient } = require('mongodb');
 const createApp = require('../src/app');
-const { generateManyBook } = require('../src/fakes/book.fake');
+const { config } = require('../src/config');
 
-jest.mock('../src/lib/mongo.lib', () => jest.fn().mockImplementation(() => ({
-  getAll: mockGetAll,
-  create: jest.fn(),
-})));
+const DB_NAME = config.dbName;
+const MONGO_URI = config.dbUrl;
 
 describe('Test for books', () => {
   let app = null;
   let server = null;
-  beforeAll(() => {
+  let database = null;
+  beforeAll(async () => {
     app = createApp();
     server = app.listen(30001);
+    const client = new MongoClient(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    await client.connect();
+    database = client.db(DB_NAME);
   });
 
   afterAll(async () => {
     await server.close();
+    await database.dropDatabase();
   });
 
   describe('test for [GET] /api/v1/books', () => {
-    test('should return a list of books', () => {
+    test('should return a list of books', async () => {
       // Arrange
-      const fakeBooks = generateManyBook(3);
-      mockGetAll.mockResolvedValue(fakeBooks);
+      const seedData = await database.collection('books').insertMany([
+        {
+          name: 'Book 1',
+          year: 1998,
+          author: 'Author 1',
+        },
+        {
+          name: 'Book 2',
+          year: 1999,
+          author: 'Author 2',
+        },
+      ]);
+      console.log(seedData);
       // Act
       return request(app).get('/api/v1/books').expect(200).then(({ body }) => {
         // Assert
         console.log(body);
-        expect(body.length).toEqual(fakeBooks.length);
+        expect(body.length).toEqual(seedData.insertedCount);
       });
     });
   });
